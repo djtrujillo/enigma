a11c9c8b1807188fabb05ff2b221a6d9803b6d11
djtrujillo <danny.j.trujillo@gmail.com>
2017-07-09 17:01:44 -0600

    Add Offset Calculator

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -1,12 +1,30 @@
 require 'date'
+require 'pry'
 class OffsetCalculator
   attr_reader :key,
-              :date
+              :date,
+              :a_offset,
+              :b_offset,
+              :c_offset,
+              :d_offset,
+              :a_rotation,
+              :b_rotation,
+              :c_rotation,
+              :d_rotation
 
   def initialize(key)
     @key = key
     @date = Date.new
     @squared_date = 0
+    @a_offset
+    @b_offset
+    @c_offset
+    @d_offset
+    @a_rotation
+    @b_rotation
+    @c_rotation
+    @d_rotation
+
   end
 
   def reformat_date
@@ -32,10 +50,17 @@ class OffsetCalculator
   def find_offsets
     last_four_string = last_4_digits.to_s
 
-    a_offset = last_four_string[-4].to_i
-    b_offset = last_four_string[-3].to_i
-    c_offset = last_four_string[-2].to_i
-    d_offset = last_four_string[-1].to_i
+    @a_offset = last_four_string[0].to_i
+    @b_offset = last_four_string[1].to_i
+    @c_offset = last_four_string[2].to_i
+    @d_offset = last_four_string[3].to_i
+  end
+
+  def add_offsets_to_rotation
+    @a_rotation = @key.a + @a_offset
+    @b_rotation = @key.b + @b_offset
+    @c_rotation = @key.c + @c_offset
+    @d_rotation = @key.d + @d_offset
   end
 
 
