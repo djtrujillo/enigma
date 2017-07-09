6e590716db475908f97eda41bb45b06e44de30dd
djtrujillo <danny.j.trujillo@gmail.com>
2017-07-09 16:20:51 -0600

    Add Key_generator back

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -10,7 +10,7 @@ class KeyGenerator
     @a = @key.values_at(0,1).join('').to_i
     @b = @key.values_at(1,2).join('').to_i
     @c = @key.values_at(2,3).join('').to_i
-    # @d = @key.values_at[3,4].join('').to_i
+    @d = @key.values_at[3,4].join('').to_i
   end
 
 
