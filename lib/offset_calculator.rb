1b9ea69a4bbb1dca0ee3932114d9a1e7654964f6
djtrujillo <danny.j.trujillo@gmail.com>
2017-07-10 19:34:00 -0600

    Fixing merge issue

--- lib/offset_calculator.rb
+++ lib/offset_calculator.rb
@@ -9,19 +9,10 @@ class OffsetCalculator
 
   def initialize(key, date = Date.today)
     @key = key
-<<<<<<< HEAD
     @date = date
-=======
-    # @a_rotation
-    # @b_rotation
-    # @c_rotation
-    # @d_rotation
-
->>>>>>> 77afc359b3c9e5c718391feb472822dc72ee7f9f
   end
 
   def reformat_date
-    @date
     string = "#{@date.mday}#{@date.mon.to_s.rjust(2, "0")}#{@date.year.to_s[-2..-1]}"
     string.to_i
   end
