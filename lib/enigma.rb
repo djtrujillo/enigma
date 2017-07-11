8e73dcdf0e5d1550ffa095d74ac6c91f347c67a0
djtrujillo <danny.j.trujillo@gmail.com>
2017-07-11 13:23:04 -0600

    Get encrypt and decrypt working

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -3,48 +3,59 @@ require 'pry'
 class Enigma
   attr_accessor :character_map
 
-  def initialize
-    @character_map = [*("a".."z"), *(0..9), " ", ".", ","]
+  def initialize # key = KeyGenerator.new, offset = OffsetCalculator.new
+    @character_map = [*("a".."z"), *("0".."9"), " ", ".", ","]
+    # @key = key
+    # @offset = offset
+
   end
 
   def cipher(rotation)
     rotated_characters = @character_map.rotate(rotation)
-    Hash[@character_map.zip(rotated_characters)]
+    hash = Hash[@character_map.zip(rotated_characters)]
   end
 
   def encrypt_letter(letter, rotation)
     cipher_for_rotation = cipher(rotation)
     cipher_for_rotation[letter]
+
+  end
+
+  def decrypt_letter(letter, rotation)
+    cipher_for_rotation = cipher(rotation)
+    cipher_for_rotation.key(letter)
   end
 
   def encrypt (message, offset_calculator)
     message_array = message_array(message)
-    encrypted_string = message_array.map do |letter|
-      encrypt_letter(letter, assign_letter_rotation(message_array, letter, offset_calculator))
+    encrypted_array = message_array.each_with_index.map do |letter, index|
+      encrypt_letter(letter, assign_letter_rotation(index, offset_calculator))
     end
-    encrypted_string.join
+    encrypted_array.join
   end
 
+  def decrypt (message, offset_calculator)
+    message_array = message_array(message)
+    decrypted_string = message_array.each_with_index.map do |letter, index|
+      decrypt_letter(letter, assign_letter_rotation(index, offset_calculator))
+    end
+    decrypted_string.join
+  end
 
-  def assign_letter_rotation(message_array, letter, offset_calculator )
-    if message_array.index(letter)    % 4 == 0
+  def assign_letter_rotation(index, offset_calculator)
+    if index    % 4 == 0
       rotation = offset_calculator.a_rotation
-    elsif message_array.index(letter) % 4 == 1
+    elsif index % 4 == 1
       rotation = offset_calculator.b_rotation
-    elsif message_array.index(letter) % 4 == 2
+    elsif index % 4 == 2
       rotation = offset_calculator.c_rotation
-    elsif message_array.index(letter) % 4 == 3
+    elsif index % 4 == 3
       rotation = offset_calculator.d_rotation
     else
     end
     rotation
   end
 
-  def decrypt(output)
-  end
-
-  def crack
-  end
 
   def message_array(message)
     message_array = message.split('')
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -5,13 +5,12 @@ require 'pry'
 require './lib/offset_calculator'
 require './lib/key_generator'
 
-
 class EncryptTest <Minitest::Test
 
   def test_the_character_map_exists
     map = Enigma.new
 
-    expected = [*("a".."z"), *(0..9), " ", ".", ","]
+    expected = [*("a".."z"), *("0".."9"), " ", ".", ","]
     actual = map.character_map
 
     assert_equal expected, actual
@@ -36,26 +35,123 @@ class EncryptTest <Minitest::Test
     assert_equal expected, actual
   end
 
-  def test_encrypt_message
-    skip
+  def test_encrypt_different_letter_with_different_rotation
     e = Enigma.new
-    key = KeyGenerator.new([1,2,3,4,5])
+    letter = "1"
+
+    expected = " "
+    actual = e.encrypt_letter(letter, 9)
+
+    assert_equal expected, actual
+  end
+
+  def test_encrypt_message
+    e = Enigma.new
+    key = KeyGenerator.new([1,1,1,1,1])
     offset_calculator = OffsetCalculator.new(key)
     offset_calculator.run_methods
-    message = "hello"
+    message = "he"
 
     actual = e.encrypt(message, offset_calculator)
 
-    assert_equal actual, "x2567"
+    assert_equal actual, "wp"
   end
 
+  def test_encrypt_message_with_differet_key
+    e = Enigma.new
+    key = KeyGenerator.new([1,2,3,4,5])
+    offset_calculator = OffsetCalculator.new(key, Date.new(2017,7,10))
+    offset_calculator.run_methods
+    message = "he"
+
+    actual = e.encrypt(message, offset_calculator)
+
+    assert_equal actual, "x1"
+  end
+
+  def test_decrypt_message
+    e = Enigma.new
+    key = KeyGenerator.new([1,1,1,1,1])
+    offset_calculator = OffsetCalculator.new(key)
+    offset_calculator.run_methods
+    message = "wp"
+
+    actual = e.decrypt(message, offset_calculator)
+
+    assert_equal actual, "he"
+  end
+
+  def test_cipher_method_returns_hash
+    e = Enigma.new
+    cipher = e.cipher(2)
+
+    expected = true
+    actual = cipher.is_a?(Hash)
+
+    assert_equal expected, actual
+  end
+
+  def test_encrypt_and_decrypt_hello_world
+    e = Enigma.new
+    key = KeyGenerator.new([3,2,3,4,5])
+    offset_calculator = OffsetCalculator.new(key, Date.new(2017,7,10))
+    offset_calculator.run_methods
+    message = "hello world"
+
+    output = e.encrypt(message, offset_calculator)
+    actual = e.decrypt(output, offset_calculator)
+
+    assert_equal message, actual
+  end
+
+  # rotation tests
+  # key of 32345
+  # date of 7/10/17
+  # a = 36
+  # b = 23
+  # c = 42
+  # d = 54
 
   # rotation tests
   # key of 12345
+  # date of 7/10/17
   # a = 16
   # b = 23
   # c = 42
   # d = 54
 
+  # rotation tests
+  # key of 11111
+  # date 7/10/17
+  # a = 15
+  # b = 11
+  # c = 19
+  # d = 20
+
+  # rotation tests
+  # key of 22222
+  # date 7/10/17
+  # a = 26
+  # b = 22
+  # c = 30
+  # d = 31
+
+  # rotation tests
+  # key of 12131
+  # date 7/10/17
+  # a = 16
+  # b = 21
+  # c = 21
+  # d = 40
+  #
+  # def test_find_rotation_numbers
+  #   key = KeyGenerator.new ([3,2,3,4,5])
+  #   offset = OffsetCalculator.new(key, Date.new(2017, 07, 10))
+  #   offset.run_methods
+  #   puts offset.a_rotation.inspect
+  #   puts offset.b_rotation.inspect
+  #   puts offset.c_rotation.inspect
+  #   puts offset.d_rotation.inspect
+  # end
 
 end
